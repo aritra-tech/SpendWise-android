c0b894ed66032ce28b00b394ef15ca793ab73fa6
aritra <aritrarick2002@gmail.com>
2024-10-02 20:14:11 +0530

    Added Compose Compiler

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.google.firebase.crashlytics) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
